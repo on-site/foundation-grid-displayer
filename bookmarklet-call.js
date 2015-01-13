3f9b847dada5515d003a3cb2edcdd6f6db93e7b0
Ben Ellis <benissimo@gmail.com>
2015-01-13 11:42:36 +0100

    URL fixes
    moved to
    https://on-site.github.io/foundation-grid-displayer/stylesheets/gd-bookm
    arklet.min.css, etc.

--- bookmarklet-call.js
+++ bookmarklet-call.js
@@ -3,6 +3,6 @@ javascript:
   var head = document.getElementsByTagName("head")[0];
   var bklScript = document.createElement("script");
   bklScript.type = "text/javascript";
-  bklScript.src = "https://raw.githubusercontent.com/on-site/foundation-grid-displayer/gh-pages/gd-bookmarklet.js";
+  bklScript.src = "https://on-site.github.io/foundation-grid-displayer/gd-bookmarklet.js";
   head.appendChild(bklScript);
 })();
